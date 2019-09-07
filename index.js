145b83f341429ce56b770a802783f26e80b5b0f8
Cpt_Crunch <jasong84@gmail.com>
2019-09-07 11:28:27 -0500

    Added Birthday Card endpoint with route and query params

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/memes', (req, res) => res.send("Hello. Welcome to Reddit!"))
+app.get('/birthdaycard/:username', (req,res) => {
+     let name = req.params.username
+     let hasConfetti = req.query.confetti === 'true';
+     let confetti =  hasConfetti ? "##$%#$%##$@#$%#$%$#$%#$%" : "";
+
+     res.send(`Happy Birthday ${name}! ${confetti} ${confetti}`)
+    })
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
