478b97e1897f68132f2216267110f9d7a79444b3
Cpt_Crunch <jasong84@gmail.com>
2019-09-07 11:14:46 -0500

    Added one more end point

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/memes', (req, res) => res.send("Hello. Welcome to Reddit!"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
